7eb494ec1ef828abea8d7a1067921fff435e56b1
poly <mcx_221@foxmail.com>
2013-04-24 17:04:47 +0800

    initial commit

---
+++ axon/axon.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import os
+try:
+    import json
+except ImportError:
+    import simplejson as json
+import struct
+import socket
+import time
+
+DEFAULT_TIMEOUT = 10
+
+class Axon(object):
+    head_patt = struct.Struct('>I')
+
+    def connect(self, addr, timeout=DEFAULT_TIMEOUT):
+        self._addr = addr
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(timeout)
+        sock.connect(addr)
+        self._sock = sock
+
+    def push(self, *args):
+        if(len(args)>1):
+            chunks = []
+            for arg in args:
+                chunks.append(self.serialize(arg))
+            self.send(self.serialize(''.join(chunks), meta=0))
+        else:
+            self.send(self.serialize(args[0]))
+
+    def send(self, msg):
+        self._sock.send(msg)
+
+    def serialize(self, msg, meta=2):
+        # see https://github.com/visionmedia/axon#protocol for more details
+        # if not isinstance(msg, basestring):
+        if meta:
+            msg = json.dumps(msg)
+        if len(msg)>(1<<24):
+            raise ValueError('the payload is too large')
+        return self.head_patt.pack(len(msg)|meta<<24)+msg
+
+if __name__=='__main__':
+    client = Axon()
+    client.connect(('127.0.0.1', 7777))
+    client.push('log', {'source':'source', 'space': 'space', 'message': 'message'})
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+VERSION = '0.1'
+
+setup(
+    name = 'node-axon',
+    packages = ['axon'], 
+    version = '0.1',
+    description = "A library to communicate with visionmedia's axon",
+    author = 'polyrabbit',
+    author_email = 'poly@wiredcraft.com',
+    url = 'https://github.com/devo-ps/python-node-axon',
+)
