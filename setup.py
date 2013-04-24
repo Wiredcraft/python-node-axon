069ff398d8c5db31b8939ac64bcbd44e1006287a
poly <mcx_221@foxmail.com>
2013-04-24 19:32:27 +0800

    tag 0.2

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author = 'polyrabbit',
     author_email = 'poly@wiredcraft.com',
     url = 'https://github.com/devo-ps/python-node-axon',
-    download_url = 'https://github.com/devo-ps/python-node-axon/archive/0.1.tar.gz',
+    download_url = 'https://github.com/devo-ps/python-node-axon/archive/0.2.tar.gz',
 )
