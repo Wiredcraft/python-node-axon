fa4765f80dc3e374d22fe9cdc7d3309f5cf760de
poly <mcx_221@foxmail.com>
2013-04-24 19:28:29 +0800

    tag 0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
-VERSION = '0.1'
+VERSION = '0.2'
 
 setup(
     name = 'axon',
     packages = ['axon'], 
-    version = '0.1',
+    version = VERSION,
     description = "A library to communicate with visionmedia's axon",
     author = 'polyrabbit',
     author_email = 'poly@wiredcraft.com',
